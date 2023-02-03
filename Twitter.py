02902bbe5a712b572af966c0cc5ad9e71bdb4eac
veena-a24 <123075812+veena-a24@users.noreply.github.com>
2023-02-03 22:32:26 +0530

    Update Twitter.py

--- Twitter.py
+++ Twitter.py
@@ -6,13 +6,13 @@ import streamlit as st
 # tweeter data scrapping as per keyword or hashtag
 data1=[]
 with st.form("my_form"):
-    search= st.text_input("Enter a keyword or hashtag based on that daya should be scrapped: ")
-    number_tweets=st.slider('Enter the count of tweets to be scrapped:', 0,1000,100)
+    search= st.text_input("Enter a keyword or hashtag based on that data should be scraped: ")
+    number_tweets=st.slider('Enter the count of tweets to be scraped:', 0,1000,100)
     num_tweet=int(number_tweets)
     submit=st.form_submit_button("Submit")
     if submit:
         for i,tweet in enumerate(sntwit.TwitterSearchScraper(search).get_items()):
-            if i>1000:
+            if i>num_tweet:
                 break
             data1.append([tweet.date,tweet.id,tweet.url,tweet.content,tweet.user.username,tweet.replyCount,tweet.retweetCount,tweet.lang,tweet.likeCount,tweet.source])                                   
 df=pd.DataFrame(data1,columns=["Date","ID","URL","Content","User_name","Reply_count","Retweet_count","Language","Likes_count","Source"])
@@ -21,15 +21,13 @@ with st.form("form"):
     st.write("Press Enter to upload the data into database:")
     Enter=st.form_submit_button("Enter")
     if Enter:
-        data=df.to_dict("records")
         client=MongoClient("mongodb://localhost:27017/")
+        df.reset_index(inplace=True)
+        data_dict = df.to_dict("records")
         db=client["tweeter_database"]
         collection=db[search]
-        db.collection.insert_many(data)
+        collection.insert_one({"index":f"{search}","data":data_dict})
         st.success("Data has been uploaded:",icon='✅')
-        st.write("list of collection names:")
-        mycoll=db.list_collection_names()
-        st.write(mycoll)
 st.write("Download the file in the format you want:")
 st.write("Download in csv format")
 def convert_to_csv(df1):
